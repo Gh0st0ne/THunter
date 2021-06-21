dea79e9a1d7df23770e82b7417015723ef8d7957
Isuru Umayanga <72663288+isuruwa@users.noreply.github.com>
2021-06-21 19:47:17 +0530

    Update thunter.py

--- thunter.py
+++ thunter.py
@@ -2,19 +2,6 @@
 # -*- coding: utf-8 -*-
 #CODED BY ISURUWA
 
-#--- Color  ---#
-W  = '\033[0m'  # white (default)
-R  = '\033[31m' # red
-G  = '\033[1;32m' # green bold
-O  = '\033[33m' # orange
-B  = '\033[34m' # blue
-P  = '\033[35m' # purple
-C  = '\033[36m' # cyan
-GR = '\033[37m' # gray
-
-
-
-
 ##################### importing modules ################################
 
 import os,sys,time
